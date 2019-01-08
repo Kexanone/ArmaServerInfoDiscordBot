83cde1c38609026ad53e80bb95bc70b2c830990f
oOKexOo <rc_mapper@yahoo.com>
2019-01-08 05:25:59 +0100

    Adjusting main script based on previous API changes

--- ArmaServerInfoDiscordBot.py
+++ ArmaServerInfoDiscordBot.py
@@ -2,11 +2,12 @@
 
 import discord
 from discord.ext import commands
+import websockets
 import asyncio
 from arma_server_query import ArmaServer
 import sys, re
 import yaml
-from operator import attrgetter
+from operator import itemgetter
 from datetime import datetime
 
 # default discord parameters
@@ -33,18 +34,18 @@ class ArmaServerInfoDiscordBot(commands.Bot):
 		
 		# set attributes
 		self.bot_token = bot_token
-		self.arma_server = ArmaServer
+		self.arma_server = arma_server
 		self.info_update_timeout = info_update_timeout
 		self.channel = discord.Object(id=channel_id)
 		try:
-			with open(STORAGE_FILE, "rb") as file:
+			with open(CHANNEL_FILE, "rb") as file:
 				message_id = str(int.from_bytes(file.read(), byteorder="big"))
 		except FileNotFoundError:
 			message_id = ""
 		self.message = discord.Object(id=message_id)
 		
 		# add background process
-		self.loop.create_task(self.backgroundProcInfoUpdate())
+		self.loop.create_task(self.worker())
 		
 		# add event handlers
 		@self.event
@@ -54,83 +55,91 @@ class ArmaServerInfoDiscordBot(commands.Bot):
 			'''
 			print("Logged in as", self.user.name)
 		
-		@self.command()
-		async def update():
-			'''
-			!update command: Posts server status
-			'''
-			self.ArmaServer.updateInfo()
-			embed = self.returnLatestStatus()
-			await self.send_status(embed=embed)
-	
 	def run(self, *args, **kwargs):
 		'''
 		starts the bot
 		'''
 		return super().run(self.bot_token, *args, **kwargs)
 	
-	async def send_status(self, *args, **kwargs):
+	async def create_message(self, *args, **kwargs):
 		return await super().send_message(self.channel, *args, **kwargs)
 		
-	async def edit_status(self, *args, **kwargs):
+	async def update_message(self, *args, **kwargs):
 		return await super().edit_message(self.message, *args, **kwargs)
 	
-	async def backgroundProcInfoUpdate(self):
+	async def worker(self):
 		'''
 		Background process that updates the server status regularly
 		'''
 		await self.wait_until_ready()
 		while(not self.is_closed):
-			# To Do: make updateInfo suspendable
-			self.ArmaServer.updateInfo()
-			if self.ArmaServer.online:
-				map = self.ArmaServer.map if self.ArmaServer.map else "?"
-				BotGame = "Zeus on {} ({}/{})".format(map, *self.ArmaServer.playerNumbers)
-				await self.change_presence(game=discord.Game(name=BotGame), status="online")
-			else:
-				await self.change_presence(game=None, status="dnd")
-			await self.postLatestStatus()
-			await asyncio.sleep(self.infoUpdateTimeout)
+			await self.send_status()
+			await asyncio.sleep(self.info_update_timeout)
 	
-	def returnLatestStatus(self):
+	async def send_status(self):
+		embed, presence = self.get_status()
+		try:
+			await self.change_presence(**presence)
+			try:
+				self.message = await self.get_message(self.channel, self.message.id)
+				await self.update_message(embed=embed)
+			except discord.errors.NotFound:
+				self.message = await self.create_message(embed=embed)
+				with open(CHANNEL_FILE, "wb") as file:
+					file.write(int(self.message.id).to_bytes(8, byteorder="big"))
+		except discord.errors.HTTPException as message:
+			perror("Discord HTTP error:", message)
+	
+	def get_status(self):
+		server_info = self.arma_server.info()
+		player_into_list = self.arma_server.players()
+		presence = self.format_presence(server_info)
+		embed = self.format_embed(server_info, player_into_list)
+		return (embed, presence)
+	
+	def format_presence(self, server_info):
+		presence = {"game": None, "status": "dnd"}
+		if server_info:
+			map = server_info["map"]
+			if not map:
+				map = "nowhere"
+			bot_game = "Zeus on {map} ({player_count}/{player_limit})".format(**server_info)
+			presence["game"] = discord.Game(name=bot_game)
+			presence["status"] = "online";
+		return presence
+		
+	def format_embed(self, server_info, player_info_list):
 		underscore = 44*"─"
-		if self.ArmaServer.online:
+		if server_info:
 			status = "Online"
 			color = 0x43B581
 		else:
 			status = "Offline"
 			color = 0xF04747
-		name = self.ArmaServer.name if self.ArmaServer.name else "Unknown Server Name"
+		name = server_info["name"]
+		if not name:
+			name = "Unknown Server Name"
 		embed = discord.Embed(title=name, description=underscore, color=color)
 		currentTime = datetime.utcnow().strftime("%H:%M:%S")
 		embed.add_field(name="Query Time:", value="{} (UTC)".format(currentTime), inline=False)
 		embed.add_field(name="Status:", value=status, inline=False)
-		embed.add_field(name="Address:", value="steam://connect/{}:{}".format(*self.ArmaServer.query.address), inline=False)
-		map = self.ArmaServer.map if self.ArmaServer.map else "none" 
+		embed.add_field(name="Address:", value="steam://connect/{}:{}".format(*self.arma_server.address), inline=False)
+		map = server_info["map"]
+		if not map:
+			map = "nowhere"
 		embed.add_field(name="Map:", value=map, inline=False)
-		mission = self.ArmaServer.mission if self.ArmaServer.mission else "none" 
+		mission = server_info["mission"] 
+		if not mission:
+			mission = "none"
 		embed.add_field(name="Mission:", value=mission, inline=False)
-		embed.add_field(name="Player Count:", value="{}/{}".format(*self.ArmaServer.playerNumbers), inline=False)
-		if len(self.ArmaServer.playerList) > 0:		
+		embed.add_field(name="Player Count:", value="{player_count}/{player_limit}".format(**server_info), inline=False)
+		if len(player_info_list) > 0:		
 			lines=["```py"]
-			for player in sorted(self.ArmaServer.playerList, key=attrgetter("name")):
-				lines.append("• {} ({})".format(player.name, player.time, player.score, ))
+			for player_info in sorted(player_info_list, key=itemgetter("name")):
+				lines.append("• {name} ({time})".format(**player_info))
 			lines.append("```")
 			embed.add_field(name="Player List:", value="\n".join(lines), inline=False)
 		return embed
-	
-	async def postLatestStatus(self):
-		embed = self.returnLatestStatus()
-		try:
-			try:
-				self.message = await self.get_message(self.channel, self.message.id)
-				await self.edit_status(embed=embed)
-			except discord.errors.NotFound:
-				self.message = await self.send_status(embed=embed)
-				with open(STORAGE_FILE, "wb") as file:
-					file.write(int(self.message.id).to_bytes(8, byteorder="big"))
-		except discord.errors.HTTPException as message:
-			perror("Discord HTTP error:", message)
 
 if __name__ == "__main__":
 	# load configuration file
@@ -143,10 +152,22 @@ if __name__ == "__main__":
 	# create instance of the Arma Server Query API
 	arma_server = ArmaServer(config["arma_server"]["address"], max_response_time=config["arma_server"]["max_response_timeout"])
 	# create and run the bot
-	bot = ArmaServerInfoDiscordBot(config["discord_bot"]["token"], arma_server, command_prefix=COMMAND_PREFIX, channelId=config["discord_bot"]["channel_id"], infoUpdateTimeout=config["discord_bot"]["info_update_timeout"])
+	bot = ArmaServerInfoDiscordBot(config["discord_bot"]["token"], arma_server, command_prefix=COMMAND_PREFIX, channel_id=config["discord_bot"]["channel_id"], info_update_timeout=config["discord_bot"]["info_update_timeout"])
+	print("Starting the bot...")
 	try:
-		bot.run()
+		while True:
+			try:
+				bot.run()
+			# Captures ConnectionResetError, which is caused when Discord shuts down the Bot's connection
+			except (ConnectionError, websockets.exceptions.ConnectionClosed) as message:
+				perror("Discord connection error:", message)
+				print("Restarting the bot...")
 	except discord.errors.LoginFailure as message:
 		perror("Discord login error:", message)
+		sys.exit(1)
 	except discord.ext.commands.errors.CommandNotFound as message:
 		perror("Discord command error:", message)
+		sys.exit(1)
+	except KeyboardInterrupt:
+		print("Bot is shutting down by request.")
+		sys.exit(0)
--- arma_server_query/server.py
+++ arma_server_query/server.py
@@ -4,60 +4,77 @@ from .query_types import *
 from .default_params import *
 import struct
 
+def decode(str, encoding, errors="replace"):
+	'''
+	Decodes while replacing erroneous data by default
+	'''
+	return str.decode(encoding, errors=errors)
+
 class SteamServer:
 	'''
 	Description:
-		Class for creating Steam server queries
+		Class for creating Steam server queries.
 	
-	Parameters:
-		address	<tuple>: the Steam server address which consists of:
-			ip <str>: IPv4
-			port <int>: Steam query port (four digit integer)
-		**kwargs <any>: for setting instance attributes
+	Args:
+		address	<tuple>: The Steam server address which consists of:
+			ip <str>: IPv4.
+			port <int>: Steam query port; a four digit integer.
 	
-	Attributes:
-		client <socket.socket>:
-		max_response_time <int|float>: maximal timeout for the response
-		buffer_size <int>: buffer size for the query response
+	Kwargs:
+		**kwargs <any>: For setting instance attributes.
+	
+	Instance Attributes:
+		client <socket.socket>: The client socket (default=new_socket).
+		max_response_time <int|float>: Maximal timeout for the
+			response (default=MAX_RESPONSE_TIME).
+		buffer_size <int>: Buffer size for the query response
+			(default=BUFFER_SIZE).
 		
 	'''
 	def __init__(self, address, **kwargs):
-		self.address = address
+		self.address = tuple(address)
 		kwargs.setdefault("max_response_time", MAX_RESPONSE_TIME)
 		kwargs.setdefault("buffer_size", BUFFER_SIZE)
 		kwargs.setdefault("client", socket(AF_INET, SOCK_DGRAM))
 		self.__dict__.update(kwargs)
-		self.client.settimeout(max_response_time)
+		self.client.settimeout(self.max_response_time)
 	
 	def A2S_info(self):
 		'''
 		Description:
-			Returns the raw response of a A2S_INFO query
+			Returns the raw response of a A2S_INFO query.
 		
-		Parameters:
+		Args:
+			none
+		
+		Kwargs:
 			none
 		
 		Returns:
-			response <bytes>: response of the query; empty when failed
+			response <bytes>: Response of the query; empty when failed.
 		'''
 		# data query
 		try:
 			self.client.sendto(A2S_INFO , self.address)
 			response, _ = self.client.recvfrom(self.buffer_size)
 		except timeout:
+			self.online = False
 			return(b"")
 		return(response)
 	
 	def A2S_players(self):
 		'''
 		Description:
-			Returns the raw response of a A2S_PLAYER query
+			Returns the raw response of a A2S_PLAYER query.
 		
-		Parameters:
+		Args:
+			none
+		
+		Kwargs:
 			none
 		
 		Returns:
-			response <bytes>: response of the query; empty when failed
+			response <bytes>: Response of the query; empty when failed.
 		'''
 		# challenge query
 		try:
@@ -80,19 +97,21 @@ class SteamServer:
 class ArmaServer(SteamServer):
 	'''
 	Description:
-		Class for creating Arma 3 server queries
+		Class for creating Arma 3 server queries.
 	
-	Parameters:
-		address	<tuple>: the server address
-			ip <str>: IPv4
-			port <int>: Steam query port (four digit integer)
-		**kwargs <any>: for setting instance attributes of the parent class
+	Args:
+		address	<tuple>: the server address:
+			ip <str>: IPv4.
+			port <int>: Steam query port; a four digit integer.
+	Kwargs:
+		**kwargs <any>: For setting instance attributes.
 	
-	Attributes:
-		max_response_time <int|float> (optional): maximal timeout for the response
-		buffer_size <int> (optional): buffer size for the query response
-		client <socket.socket> (optional): client socket for the connection
-	
+	Instance Attributes:
+		client <socket.socket>: The client socket (default=new_socket).
+		max_response_time (<int>|<float>): Maximal timeout for the
+			response (default=MAX_RESPONSE_TIME).
+		buffer_size <int>: Buffer size for the query response
+			(default=BUFFER_SIZE).
 	'''
 	def __init__(self, address, **kwargs):
 		super().__init__(address, **kwargs)
@@ -100,19 +119,23 @@ class ArmaServer(SteamServer):
 	def info(self):
 		'''
 		Description:
-			Returns a dict from a response of a A2S_INFO query
+			Returns a dict from a response of a A2S_INFO query.
 		
-		Parameters:
+		Args:
+			none
+		
+		Kwargs:
 			none
 		
 		Returns:
-			response <dict>: with the possible keys:
-				name <str>: the name of the server
-				map <str>: the name of the map
-				mission <str>: the name of the mission
-				player_numbers <tuple>: the name of the mission
-					player_count <int>: # players on the server
-					player_max_count <int>: player limit for the server
+			response <dict>: With the possible keys:
+				name <str>: The name of the server.
+				map <str>: The name of the map.
+				mission <str>: The name of the mission.
+				player_numbers <tuple>: The player counts:
+					player_count <int>: # players on the server.
+					player_max_count <int>: Player limit for the
+						server.
 		'''
 		# get the byte string response
 		response = self.A2S_info()
@@ -123,22 +146,21 @@ class ArmaServer(SteamServer):
 			idx_start = 1 + response.find(b"\x11")
 			idx_end = response.find(b"\x00", idx_start)
 			name = response[idx_start:idx_end]
-			data["name"] = name.decode("UTF-8")
+			data["name"] = decode(name, "UTF-8")
 			# get map
 			idx_start = 1 + idx_end
 			idx_end = response.find(b"\x00", idx_start)
 			map = response[idx_start:idx_end]
-			data["map"] = map.decode("UTF-8")
+			data["map"] = decode(map, "UTF-8")
 			# get mission
 			idx_start = 1 + response.find(b"\x00", idx_end+1)
 			idx_end = response.find(b"\x00", idx_start)
 			mission = response[idx_start:idx_end]
-			data["mission"] = mission.decode("UTF-8")
+			data["mission"] = decode(mission, "UTF-8")
 			# get player numbers
 			idx = 3 + response.find(b"\x00", idx_end)
-			player_count = int.from_bytes(response[idx:idx+1], byteorder="big")
-			player_max_count = int.from_bytes(response[idx+1:idx+2], byteorder="big")
-			data["player_numbers"] = (player_count, player_max_count)
+			data["player_count"] = int.from_bytes(response[idx:idx+1], byteorder="big")
+			data["player_limit"] = int.from_bytes(response[idx+1:idx+2], byteorder="big")
 			return(data)
 		else:
 			return({})
@@ -146,16 +168,21 @@ class ArmaServer(SteamServer):
 	def players(self):
 		'''
 		Description:
-			Returns a dict from a response of a A2S_PLAYER query
+			Returns a dict from a response of a A2S_PLAYER query.
 		
-		Parameters:
+		Args:
+			none
+		
+		Kwargs:
 			none
 		
 		Returns:
-			response <list> of <dict>, where each dict represents a player:
-				name <str>: the name of the player
-				score <str>: total score
-				time <str>: the time since the player joined in the format hh:mm
+			response (<list> of <dict>): Where a dict represents a
+				player:
+					name <str>: The name of the player.
+					score <str>: Total score.
+					time <str>: The time since the player joined in the
+						format hh:mm.
 		'''
 		# get the byte string response
 		response = self.A2S_players()
@@ -171,7 +198,7 @@ class ArmaServer(SteamServer):
 				if idx_end < 0:
 					break
 				name = response[idx_start:idx_end]
-				player_data = {"name": name.decode("UTF-8")}
+				player_data = {"name": decode(name, "UTF-8")}
 				# get score
 				idx = 1 + idx_end
 				# exit when date is incomplete
